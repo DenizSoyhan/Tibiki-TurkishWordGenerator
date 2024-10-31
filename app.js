39833e61642d48f0c906142432a4b8e8b03d73f1
denizsoyhan <denizsoyhan@gasai.kali>
2024-10-31 23:58:11 +0300

    J harfi kalktı, büyük ünlü uyumu var, tek sessiz harften oluşan kelimeler artık yok.

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const writeStream = fs.createWriteStream('Output.txt');
 
 var KSH = ['a','ı','o','u']; //kalın sesli harfler
 var ISH = ['e','i','ö','ü']; //ince sesli harfler
-var SZH = ['b', 'c', 'ç', 'd', 'g', 'ğ', 'j', 'h', 'k', 'l'
+var SZH = ['b', 'c', 'ç', 'd', 'g', 'ğ', 'h', 'k', 'l'  //J harfi olmaz türkçe kelimelerde
           ,'m', 'n', 'p', 'r', 's', 'ş', 't', 'v', 'y', 'z'];
 
-var startSZH = ['b', 'c', 'ç', 'd', 'g', 'j', 'k', 'l'
+var startSZH = ['b', 'c', 'ç', 'd', 'g', 'k', 'l' //J harfi olmaz türkçe kelimelerde
               , 'n', 'p', 'r', 's', 'ş', 't', 'v', 'y', 'z']; //a word can't start with m h ğ 
 
 function getRandomIntInRange(min, max) {
@@ -23,62 +23,101 @@ function generateWord(){
     var nextLetter;//1 ünlü 0 ünsüz 
     var firstLetterDecided=0;
 
-    if (decideFirstLetter==1){
+    var firstKalinORince; //1 kalın 0 ince
+
+    if (decideFirstLetter==1 && !firstLetterDecided){//ÜNLÜ İLE BAŞLAR KELİME
         var KSHorISH=getRandomIntInRange(0,1); //1 kalın - 0 ünlü
+        firstKalinORince=KSHorISH;
          //syllables can be 3 letters long
-        if(KSHorISH==1){
-            syllable.push(KSH[getRandomIntInRange(0,KSH.length-1)]);
+        if(KSHorISH==1){ //KALIN ÜNLÜ HARF
+            let controll=KSH[getRandomIntInRange(0,KSH.length-1)]
+            syllable.push(controll);
+            //console.log(controll)
             nextLetter=0;
-        }else if(KSHorISH==0){
-            syllable.push(KSH[getRandomIntInRange(0,ISH.length-1)]);
+            //İLK HECEDE KALIN ÜNLÜ VAR
+        }else if(KSHorISH==0){//İNCE ÜNLÜ HARF
+            syllable.push(ISH[getRandomIntInRange(0,ISH.length-1)]);
             nextLetter=0;
+          
         }
-    }else if(decideFirstLetter==0){
+    }else if(decideFirstLetter==0 && !firstLetterDecided){//ÜNSÜZ HARF İLE BAŞLAR
+
         syllable.push(startSZH[getRandomIntInRange(0,startSZH.length-1)]);
         nextLetter=1;
+        //console.log(`kelime uzunluğu: ${wordLen}  hece uzunluğu: ${syllableLen}`)
+        if(syllableLen==1 && wordLen==1){ //eğer ilk harf sessiz ve kelime tek heceden oluşuyorsa yalnız 1 sessiz harf üretmesini engelle 
+            noOneLetterConstWord=getRandomIntInRange(0,1)//0 ise kelimeyi uzat 1 ise heceyi
+            if(noOneLetterConstWord==1){
+            syllableLen=syllableLen+Number(getRandomIntInRange(1,2));
+            }else if(noOneLetterConstWord==0){
+                wordLen=wordLen+Number(getRandomIntInRange(1,2));
+            }
+        }
     }
     
     //console.log(syllable.join(""),nextLetter);
     for(var j=0;j<wordLen;j++){
+       // console.log("kelime loop:",j, " yeni kelime uzunluğu: ", wordLen," yeni hece uzunluğu: ", syllableLen);
         if(firstLetterDecided==0){
-        var k=1;
-    }else{
-        var k=0;
-    }
+            var k=1;
+            firstLetterDecided=1;
+        }else{
+            var k=0;
+        }
     
-    for(var k=0;k<syllableLen;k++){
-        if(nextLetter==0){
+        for(;k<syllableLen;k++){
+            if(nextLetter==0){ //ÜNSÜZ HARF EKLENECEK
 
-            syllable.push(SZH[getRandomIntInRange(0,SZH.length-1)]);
-            nextLetter=1;
+                syllable.push(SZH[getRandomIntInRange(0,SZH.length-1)]);
+                nextLetter=1;
 
-        }else if(nextLetter==1){
-            KSHorISH=getRandomIntInRange(0,1); //1 kalın - 0 ünlü
+            }else if(nextLetter==1){//ÜNLÜ HARF EKLENECEK
+                //KSHorISH=getRandomIntInRange(0,1); //1 kalın - 0 ünlü
 
-            if(KSHorISH==1){
-                syllable.push(KSH[getRandomIntInRange(0,KSH.length-1)]);
-                nextLetter=0;
-            }else if(KSHorISH==0){
-                syllable.push(ISH[getRandomIntInRange(0,ISH.length-1)]);
+                if(firstKalinORince==1){//KALIN ÜNLÜ EKLENECEK ÇÜNKÜ İLK EKLENEN SESLİ HARF KALIN
+                    let letter=KSH[getRandomIntInRange(0,KSH.length-1)]
+                    syllable.push(letter);
+                    //console.log("kalin: ", letter)
+                    
+                }else if(firstKalinORince==0){//İNCE ÜNLÜ EKLENECEK ÇÜNKÜ İLK EKLENEN SESLİ HARF İNCE
+                    let letter=ISH[getRandomIntInRange(0,ISH.length-1)]
+                    syllable.push(letter);
+                    //console.log("ince: ", letter)
+                    
+                }else{
+                    
+                    firstKalinORince=getRandomIntInRange(0,1); // BURAYA GİRMESİ DEMEK İLK HARF SESSİZ HARFTİ İLK SESLİ HARF VE TÜRÜ BELİRLENECEK 
+                    //AŞAĞIDAKİLER YUKARIDAKİ KARAR MEKANİZMALARININ AYNISI
+                    if(firstKalinORince==0){//KALIN ÜNLÜ EKLENECEK ÇÜNKÜ İLK EKLENEN SESLİ HARF KALIN
+                        syllable.push(KSH[getRandomIntInRange(0,KSH.length-1)]);
+                        firstKalinORince=1;
+                       
+                    }else if(firstKalinORince==1){//İNCE ÜNLÜ EKLENECEK ÇÜNKÜ İLK EKLENEN SESLİ HARF İNCE
+                        syllable.push(ISH[getRandomIntInRange(0,ISH.length-1)]);
+                        firstKalinORince=0;
+                        
+                    }
+                //console.log("ilk harf sessizdi: ",firstKalinORince);
+                
+                }
+            
                 nextLetter=0;
             }
-            
-            nextLetter=0;
+        
+            firstLetterDecided=1;
         }
-        
-        
-    }
-    
-    firstLetterDecided=1;
+        //console.log(syllable.join(""));
     }
     //console.log(syllable);
     syllableList.push(syllable.join(""));
     var word=syllableList.join("");
     writeStream.write(`${word}\n`); 
-    console.log(word);   
+    console.log("word: ", word);   
+    
     
 }
 
-for(var i =0;i<20;i++){
+for(var i =0;i<10;i++){
     generateWord();
+    //console.log();
 }
